51668603cc13b29c92cccf4a3f5698074fdb2cde
Yaroslav <y.b.grytsishyn@student.khai.edu>
2021-10-02 11:34:42 +0300

    ChatBot{2-nd}stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,2 +1,7 @@
 print("Hello! My name is Newton\nI was created in 2021")
 # выводит текст с приветствием
+
+username = input("Please, remind me your name.\n>")
+print("What a great name you have," + username)
+# запрашивает имя и выводит текст добавляя к нему имя
+
