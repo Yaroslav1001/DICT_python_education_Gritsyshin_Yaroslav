8f968f3f3f5b73a9e5dc9ebe479328d68eb65742
Yaroslav <y.b.grytsishyn@student.khai.edu>
2021-10-03 11:27:11 +0300

    Hangman{3-rd}stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,10 +1,14 @@
+import random
+
 print("HANGMAN\nThe game will be available soon.")
 
+text = ["python", "java", "javascript", "C#"]
 while True:
     print("Hangman")
     gue = input("Guess the word:>")
-    if gue == "python":
+    if gue == (random.choice(text)):
         print("You survived!")
         break
     else:
+        print(random.choice(text))
         print("You lost!")
