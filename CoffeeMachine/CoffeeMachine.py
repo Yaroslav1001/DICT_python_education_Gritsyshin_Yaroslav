d91f930cb4cde518b067cc707859d300a7381d03
Yaroslav <y.b.grytsishyn@student.khai.edu>
2021-11-12 18:48:20 +0200

    CoffeeMachine {5th} stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -5,7 +5,7 @@ cups = 9
 money = 550
 
 
-def content():
+def content2():
     print('The coffee machine has:')
     print(str(water) + ' of water')
     print(str(milk) + ' of milk')
@@ -14,35 +14,63 @@ def content():
     print(str(money) + ' of money')
 
 
-content()
-print("")
+def enough():
+    global water
+    global milk
+    global coffee_beans
+    global money
+    if water < 0:
+        return "Sorry, not enough water"
+    elif milk < 0:
+        return "Sorry, not enough milk"
+    elif coffee_beans < 0:
+        return "Sorry, not enough coffee_beans"
+    elif cups < 0:
+        return "Sorry, not enough disposable cups"
 
 
 def buy():
-    print('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:')
+    print('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back – to main menu:')
     global water
     global milk
     global coffee_beans
     global money
     global cups
-    answer_user = int(input())
-    if answer_user == 1:
+    answer_user = input()
+    if answer_user == str(1):
         water -= 250
         coffee_beans -= 16
         cups -= 1
         money += 4
-    elif answer_user == 2:
+        if enough() == None:
+            print('I have enough resources, making you a coffee!')
+        else:
+            print(enough())
+        return 'espresso'
+    elif answer_user == str(2):
         water -= 350
         milk -= 75
         coffee_beans -= 20
         cups -= 1
         money += 7
-    elif answer_user == 3:
+        if enough() == None:
+            print('I have enough resources, making you a coffee!')
+        else:
+            print(enough())
+        return 'latte'
+    elif answer_user == str(3):
         water -= 200
         milk -= 100
         coffee_beans -= 12
         cups -= 1
         money += 6
+        if enough() == None:
+            print('I have enough resources, making you a coffee!')
+        else:
+            print(enough())
+        return 'cappuccino'
+    elif answer_user == 'back':
+        return 'back'
 
 
 def fill():
@@ -67,22 +95,32 @@ def fill():
 
 def take():
     global money
-    print("I gave you " + str(money) + "\n")
+    print("I gave you " + str(money))
     money = 0
 
 
 def action():
-    print('Write action (buy, fill, take):')
-    action_user = input()
+    print('\nWrite action (buy, fill, take, remaining, exit):')
+    action_user = str(input())
     if action_user == 'buy':
+        print("")
         buy()
-        content()
+        return 'buy'
     elif action_user == 'fill':
+        print('')
         fill()
-        content()
+        return 'fill'
     elif action_user == 'take':
         take()
-        content()
+        return 'take'
+        elif action_user == 'remaining':
+        print('')
+        remaining()
+        return 'remaining'
+    elif action_user == 'exit':
+        return 'exit'
 
+while_action = 0
 
-action()
+while while_action != 'exit':
+    while_action = action()
