78379aafd94e7f79fc50750210407e690b4be815
Yaroslav <y.b.grytsishyn@student.khai.edu>
2021-11-12 18:13:35 +0200

    CoffeeMachine {3rd} stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -3,14 +3,6 @@ print('Pouring coffee into the cup\nPouring some milk into the cup\nCoffee is re
 coffee_water = 200
 coffee_milk = 50
 coffee_beans = 15
-count_cups = int(input("\nWrite how many cups of coffee you will need:"))
-water = count_cups * coffee_water
-milk = count_cups * coffee_milk
-beans = count_cups * coffee_beans
-print('For ' + str(count_cups) + ' cups of coffee you will need:')
-print(str(water) + ' ml of water')
-print(str(milk) + ' ml of milk')
-print(str(beans) + ' g of coffee beans')
 print("Write how many ml of water the coffee machine has:")
 water = int(input())
 print("Write how many ml of milk the coffee machine has:")
