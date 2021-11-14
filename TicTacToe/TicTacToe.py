73e01fa72560e86288196c9200b6095d9e1e88e2
Yaroslav <y.b.grytsishyn@student.khai.edu>
2021-11-14 18:08:25 +0200

    TicTacToe {4th} stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,5 +1,13 @@
 print('Enter cells:')
 a = list(input())
+lim = str('---------')
+l_1 = ['|', a[0], a[1], a[2], '|']
+l_2 = ['|', a[3], a[4], a[5], '|']
+l_3 = ['|', a[6], a[7], a[8], '|']
+dig = [int(i) for i in range(1,4)]
+s_l = [' ', l_1, l_2, l_3, ' ']
+answer_1 = 0
+answer_2 = 0
 
 
 def vic():
@@ -8,22 +16,22 @@ def vic():
         return "Impossible"
     elif a.count("O") - a.count("X") >= 2:
         return "Impossible"
-    if a[0] == a[1] == a[2] != '_':
-        res.append(a[0])
-    if a[3] == a[4] == a[5] != '_':
-        res.append(a[3])
-    if a[6] == a[7] == a[8] != '_':
-        res.append(a[6])
-    if a[0] == a[3] == a[6] != '_':
-        res.append(a[0])
-    if a[1] == a[4] == a[7] != '_':
-        res.append(a[1])
-    if a[2] == a[5] == a[8] != '_':
-        res.append(a[2])
-    if a[0] == a[4] == a[8] != '_':
-        res.append(a[0])
-    if a[2] == a[4] == a[6] != '_':
-        res.append(a[2])
+    if l_1[1] == l_1[2] == l_1[3] != '_':
+        res.append(l_1[1])
+    if l_2[1] == l_2[2] == l_2[3] != '_':
+        res.append(l_2[1])
+    if l_3[1] == l_3[2] == l_3[3] != '_':
+        res.append(l_3[1])
+    if l_1[1] == l_2[1] == l_3[1] != '_':
+        res.append(l_1[1])
+    if l_1[2] == l_2[2] == l_3[2] != '_':
+        res.append(l_1[2])
+    if l_1[3] == l_2[3] == l_3[3] != '_':
+        res.append(l_1[3])
+    if l_1[1] == l_2[2] == l_3[3] != '_':
+        res.append(l_1[1])
+    if l_1[3] == l_2[2] == l_3[1] != '_':
+        res.append(l_1[3])
     if res.count('X') >= 2:
         res.remove('X')
     elif res.count('O') >= 2:
@@ -41,12 +49,36 @@ def vic():
 
 
 def grid():
-    print('---------')
-    print('| ' + a[0] + ' ' + a[1] + ' ' + a[2] + ' |')
-    print('| ' + a[3] + ' ' + a[4] + ' ' + a[5] + ' |')
-    print('| ' + a[6] + ' ' + a[7] + ' ' + a[8] + ' |')
-    print('---------')
+    print(lim)
+    print(' '.join(l_1))
+    print(' '.join(l_2))
+    print(' '.join(l_3))
+    print(lim)
     print(vic())
 
 
+def move():
+    cycle = 0
+    global l_1, l_2, l_3, answer_1, answer_2
+    while cycle != 1:
+        print('Enter the coordinates:')
+        try:
+            answer_1, answer_2 = input().split(' ')
+            answer_1 = int(answer_1)
+            answer_2 = int(answer_2)
+        except ValueError:
+            print('You should enter numbers!')
+            continue
+        if answer_1 and answer_2 not in dig:
+            print('Coordinates should be from 1 to 3!')
+        elif answer_1 and answer_2 in dig:
+            if s_l[answer_1][answer_2] != '_':
+                print('This cell is occupied! Choose another one!')
+            else:
+                s_l[int(answer_1)][int(answer_2)] = 'X'
+                cycle = 1
+    grid()
+
+
 grid()
+move()
