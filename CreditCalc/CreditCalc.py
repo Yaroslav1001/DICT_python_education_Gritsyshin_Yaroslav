8aeca3c8cad4c0293c0466faeaa7453b1cf75f61
Yaroslav <y.b.grytsishyn@student.khai.edu>
2021-11-14 19:03:27 +0200

    CreditCalc {2nd} stage

--- CreditCalc/CreditCalc.py
+++ CreditCalc/CreditCalc.py
@@ -1,9 +1,21 @@
-def text():
-    print('Loan principal: 1000')
-    print('Month 1: repaid 250')
-    print('Month 2: repaid 250')
-    print('Month 3: repaid 500')
-    print('The loan has been repaid!')
+import math
 
+print("Enter the loan principal:")
+lon = int(input("> "))
+print("""What do you want to calculate?
+type "m" – for number of monthly payments,
+type "p" – for the monthly payment:""")
+w = input()
 
-text()
+if w == "p":
+        h = int(input("Enter the number of months: \n>"))
+        pay = math.ceil(lon /h)
+        lp = math.ceil(lon - (h - 1) * pay)
+        if lp != pay:
+                print("Your monthly payment = {pay} and the last payment = {lp}.")
+        else:
+                print(f"Your monthly payment = {pay}.")
+if w == "m":
+        m = int(input("Enter the monthly payment: \n>"))
+        payr = lon//m
+        print(f"It will take {payr} month to repay the loan. ")
