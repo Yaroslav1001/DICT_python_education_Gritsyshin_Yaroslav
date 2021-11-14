6f7153e09b2038b40ae9293d26032d1384895fab
Yaroslav <y.b.grytsishyn@student.khai.edu>
2021-11-14 18:47:13 +0200

    CreditCalc {1st} stage

---
+++ CreditCalc/CreditCalc.py
@@ -0,0 +1,9 @@
+def text():
+    print('Loan principal: 1000')
+    print('Month 1: repaid 250')
+    print('Month 2: repaid 250')
+    print('Month 3: repaid 500')
+    print('The loan has been repaid!')
+
+
+text()
