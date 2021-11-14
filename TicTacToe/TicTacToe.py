65b6fdacd4b88012f778dc3c470c95b924843ef5
Yaroslav <y.b.grytsishyn@student.khai.edu>
2021-11-14 17:43:54 +0200

    TicTacToe {2nd} stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,7 +1,11 @@
 def grid():
-    print('X', 'O', 'X')
-    print('O', 'X', 'O')
-    print('X', 'X', 'O')
+    print('Enter cells:')
+    a = list(input())
+    print('---------')
+    print('| ' + a[0] + ' ' + a[1] + ' ' + a[2] + ' |')
+    print('| ' + a[3] + ' ' + a[4] + ' ' + a[5] + ' |')
+    print('| ' + a[6] + ' ' + a[7] + ' ' + a[8] + ' |')
+    print('---------')
 
 
 grid()
