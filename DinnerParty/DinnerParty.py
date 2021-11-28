2e5281019454a5f2209eb9fd0b67463f211343b7
Yaroslav <y.b.grytsishyn@student.khai.edu>
2021-11-28 12:35:32 +0200

    DinnerParty {4-th} stage

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,32 +1,37 @@
 import random
 
-friends = int(input("Enter the number of friends joining (including you):"))
-if friends > 0:
-    print("Enter the name of every friend (including you), each on a new line:")
-    name = [input() for i in range(friends)]
-    new_dict = {name[z - 1]: 0 for z in range(1, friends + 1)}
-    amount = int(input("Enter the total amount: "))
-    new_dict_keys = list(new_dict.keys())
-    new_dict_keys.sort()
-    while True:
-        choice = input("Do you want to use the 'Who is lucky?' feature? Write Yes/No: ")
-        if choice == "Yes":
-            list_amount = [round((amount / (friends - 1)), 2)] * friends
-            lucky_friend_index = random.choice(range(friends))
-            for i in range(len(new_dict_keys)):
-                new_dict[new_dict_keys[i]] = list_amount[i]
-            new_dict[new_dict_keys[lucky_friend_index]] = 0
-            print(f"{new_dict_keys[lucky_friend_index]} is the lucky one!")
-            print(new_dict)
+while True:
+    comrades = int(input("Enter the number of friends joining (including you):"))
+    if comrades > 0:
+        print("Enter the name of every friend (including you), each on a new line:")
+        name = [input() for x in range(comrades)]
+        dict = {name[y - 1]: 0 for y in range(1, comrades + 1)}
+        price = int(input("Enter the total amount: "))
+        dk = list(dict.keys())
+        dk.sort()
+        while True:
+            choice = input("Do you want to use the 'Who is lucky?' feature? Write Yes/No: ")
+            if choice == "Yes":
+                pricelist = [round((price / (comrades - 1)), 2)] * comrades
+                lucky_friend_index = random.choice(range(comrades))
+                lucky_dude = random.choice(range(comrades))
+                for x in range(len(dk)):
+                    dict[dk[x]] = pricelist[x]
+                print(f"{dk[lucky_dude]} is the lucky one!")
+                break
+            elif choice == "No":
+                pricelist = [round((price / comrades), 2)] * comrades
+                for x in range(len(dk)):
+                    dict[dk[x]] = pricelist[x]
+                print("No one is going to be lucky.")
+                print(dict)
+                break
+            else:
+                print("Incorrect choice. Try again!")
             break
-        elif choice == "No":
-            list_amount = [round((amount / friends), 2)] * friends
-            for i in range(len(new_dict_keys)):
-                new_dict[new_dict_keys[i]] = list_amount[i]
-            print("No one is going to be lucky.")
-            print(new_dict)
-            break
-        else:
-            print("Incorrect choice. Try again!")
-else:
-    print("No one is joining for the party.")
+    elif comrades < 0:
+        print("It can't be!")
+        continue
+    else:
+        print("No one is joining for the party.")
+        break
