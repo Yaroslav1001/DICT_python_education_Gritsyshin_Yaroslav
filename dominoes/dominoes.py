ed10550ab46052f302bb4dce70cec31efe02a772
Yaroslav <y.b.grytsishyn@student.khai.edu>
2021-12-12 15:09:56 +0200

    dominoes {2-nd} stage

--- dominoes/dominoes.py
+++ dominoes/dominoes.py
@@ -1,31 +1,38 @@
-from random import random
+import random
 
-p = []
-d = [[a, b] for a in range(7) for b in range(7)]
+setr = [[a, b] for a in range(7) for b in range(a, 7)]
+box = []
 
-while len(p) != 14:
-    part = random.choice(d)
-    p.append(part)
-    d.remove(part)
-gamer = p[:7]
-machine = p[7:]
-play = []
+
+while len(box) != 14:
+    step = random.choice(setr)
+    box.append(step)
+    setr.remove(step)
+gamer = box[:7]
+machine = box[7:]
+game = []
 minus = -1
-give = [a for a in p if a[0] == a[0]]
+give = [a for a in box if a[0] == a[0]]
 
 for a in give:
     if a[0] > minus:
         minus = a[0]
-        play = a
-if play in gamer:
-    gamer.remove(play)
-    start = "player"
+        game = a
+if game in gamer:
+    gamer.remove(game)
+    start = "Computer is about to make a move. Press Enter to continue.."
 else:
-    machine.remove(play)
-    start = "computer"
+    machine.remove(game)
+    start = "It's your turn to make a move. Enter your command"
 
-print("Stock pieces: ", format(d))
-print("Computer pieces ", format(machine))
-print("Player pieces ", format(gamer))
-print("Domino snake ", format(play))
+print("Stock pieces: ", format(len(setr)))
+print("Computer pieces: ", format(len(machine)))
+print("Player pieces: ", format(len(gamer)))
+print("Domino snake: ", format(game))
+
+
+for a in range(len(gamer)):
+    print(format(a+1),':')
+    print(format(gamer[a]))
+
 print("Status", format(start))
