0aa09c0ec35d3fee3f437f8ed10d11a3bb3c6537
Yaroslav <y.b.grytsishyn@student.khai.edu>
2021-12-19 23:07:08 +0200

    rock_paper_scissors {3-rd} satge

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,14 +1,17 @@
 from random import choice
 
 print("Welcome To Rock-Paper-Scissors")
-gamer = input("Select any option\nrock\npaper\nscissors ").strip().lower()
-computer = choice("rps")
-if gamer == computer:
-    print("Draw")
-    print("Computer's choice -> ", computer)
-elif gamer == "r" and computer == "s" or gamer == "p" and computer == "r" or gamer == "s" and computer == "p":
-    print("Player Wins")
-    print("Sorry, but the computer chose another option and failed:", computer)
-else:
-    print("Enter another word")
-    print("Sorry, but the computer chose another option:", computer)
+while True:
+    gamer = input("Select any option\nrock\npaper\nscissors\nexit ").strip().lower()
+    machine = choice("rps")
+    if gamer == machine:
+        print("Draw")
+        print("Computer's choice -> ", machine)
+    elif gamer == "r" and machine == "s" or gamer == "p" and machine == "r" or gamer == "s" and machine == "p":
+        print("Player Wins")
+        print("Sorry, but the computer chose another option and failed:", machine)
+    else:
+        print("Enter another word")
+        print("Sorry, but the computer chose another option:", machine)
+    if gamer == "exit":
+        break
