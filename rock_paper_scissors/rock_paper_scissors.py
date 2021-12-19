65a3c948f45820364839d0fb7e8e016ac1f912fa
Yaroslav <y.b.grytsishyn@student.khai.edu>
2021-12-19 23:17:59 +0200

    rock_paper_scissors {4-th} satge

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,17 +1,51 @@
 from random import choice
 
 print("Welcome To Rock-Paper-Scissors")
+selection = {
+    'rock': ['gun', 'devil', 'dragon', 'water', 'air', 'paper'],
+    'paper': ['fire', 'scissors', 'wolf', 'tree', 'human', 'snake'],
+    'scissors': ['gun', 'lightning', 'devil', 'dragon', 'water', 'rock'],
+    'gun': ['water', 'air', 'sponge', 'paper', 'devil', 'dragon'],
+    'lightning': ['water', 'air', 'sponge', 'paper', 'dragon', 'wolf'],
+    'devil': ['water', 'air', 'sponge', 'paper', 'tree', 'wolf'],
+    'dragon': ['air', 'sponge', 'paper', 'tree', 'wolf', 'human'],
+    'water': ['snake', 'sponge', 'paper', 'tree', 'wolf', 'human'],
+    'air': ['snake', 'sponge', 'scissors', 'tree', 'wolf', 'human'],
+    'sponge': ['snake', 'scissors', 'tree', 'human', 'rock', 'fire'],
+    'wolf': ['snake', 'scissors', 'human', 'rock', 'fire', 'gun'],
+    'tree': ['snake', 'scissors', 'rock', 'fire', 'gun', 'lightning'],
+    'human': ['scissors', 'rock', 'fire', 'gun', 'lightning', 'devil'],
+    'snake': ['rock', 'fire', 'gun', 'lightning', 'devil', 'dragon'],
+    'fire': ['fire', 'gun', 'lightning', 'devil', 'dragon', 'water'],
+}
+
+
+def is_player_won(gamer_word, machine_word):
+    victory = True
+    for word in selection[gamer_word]:
+        if word == machine_word:
+            victory = False
+    return victory
+
+
 while True:
-    gamer = input("Select any option\nrock\npaper\nscissors\nexit ").strip().lower()
-    machine = choice("rps")
-    if gamer == machine:
+    gamer_input = input(
+        "Select any option\nrock\npaper\nscissors\ngun\nlightning\ndevil\ndragon\nwater\nair\nsponge\nwolf\ntree"
+        "\nhuman\nsnake\nfire\nexit\n ").strip()
+    machine_input = choice(
+        ['rock', 'paper', 'scissors', 'gun', 'lightning', 'devil', 'dragon', 'water', 'air', 'sponge', 'wolf', 'tree',
+         'human', 'snake', 'fire'])
+    if gamer_input == machine_input:
         print("Draw")
-        print("Computer's choice -> ", machine)
-    elif gamer == "r" and machine == "s" or gamer == "p" and machine == "r" or gamer == "s" and machine == "p":
+        print("Computer's choice -> ", machine_input)
+    elif is_player_won(gamer_input, machine_input):
         print("Player Wins")
-        print("Sorry, but the computer chose another option and failed:", machine)
+        print("Computer chose: ", machine_input)
+    elif not is_player_won(gamer_input, machine_input):
+        print("Computer Wins")
+        print("Computer chose: ", machine_input)
     else:
         print("Enter another word")
-        print("Sorry, but the computer chose another option:", machine)
-    if gamer == "exit":
+        print("Sorry, but the computer chose another option:", machine_input)
+    if gamer_input == "exit":
         break
