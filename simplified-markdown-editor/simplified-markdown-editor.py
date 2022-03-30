6e827716dda7f3f4dae74e4bbfc27ef3b0379e1b
Yaroslav <y.b.grytsishyn@student.khai.edu>
2022-03-30 14:33:32 +0300

    simplified-markdown-editor {3-rd}

--- simplified-markdown-editor/simplified-markdown-editor.py
+++ simplified-markdown-editor/simplified-markdown-editor.py
@@ -7,8 +7,8 @@ class MarkdownEditor:
     reserved_characters = ("*", "**", "```", "#")
 
     format = ("plain", "bold", "italic", "header",
-                            "link", "inline-code", "ordered",
-                            "list", "unordered-list", "new-line")
+              "link", "inline-code", "ordered",
+              "list", "unordered-list", "new-line")
 
     special_commands = "!help", "!done"
 
@@ -36,57 +36,59 @@ class MarkdownEditor:
             self.menu_options()
 
         elif option == "!done":
-            self.add_markup(self.content)
+
             return
 
         elif option == "plain":
             user_input = self.correct_input("Text: > ")
             self.content += f"{user_input} "
-            print(self.content)
-            self.menu_options()
 
         elif option == "bold":
             user_input = self.correct_input("Text: > ")
             self.content += f"**{user_input}** "
-            print(self.content)
-            self.menu_options()
 
         elif option == "italic":
             user_input = self.correct_input("Text: > ")
             self.content += f"*{user_input}* "
-            print(self.content)
-            self.menu_options()
 
         elif option == "header":
             level = self.correct_input_level("Level: > ")
             user_input = self.correct_input("Text: > ")
-            self.content += f"{level * '#'} {user_input}\n"
-            print(self.content)
-            self.menu_options()
-            ...
+
+            if not self.selected_format:
+                self.content += f"\n"
+                self.selected_format = option
+            else:
+                self.content += f"\n\n"
+
+            self.content += f"{level * '#'} {user_input}\n\n"
+
         elif option == "link":
             label = self.correct_input("Label: > ")
             url = self.correct_input("URL: > ")
             self.content += f"[{label}]({url}) "
-            print(self.content)
-            self.menu_options()
-            ...
+
         elif option == "inline-code":
             user_input = self.correct_input("Text: > ")
             self.content += f"```{user_input}``` "
-            print(self.content)
-            self.menu_options()
-        elif option == "ordered":
-            ...
-        elif option == "list":
-            ...
+
+        elif option == "ordered-list":
+            number_rows = self.input_number_rows("Number of rows: > ")
+            for row in range(1, number_rows + 1):
+                user_input = self.correct_input(f"Row #{row}: > ")
+                self.content += f"{row}. {user_input}  \n"
+
         elif option == "unordered-list":
-            ...
+            number_rows = self.input_number_rows("Number of rows: > ")
+            for row in range(1, number_rows + 1):
+                user_input = self.correct_input(f"Row #{row}: > ")
+                self.content += f"* {user_input}  \n"
+
         elif option == "new-line":
-            self.content += "\n"
-            print(self.content)
-            self.menu_options()
-            ...
+            self.content += "  \n"
+
+        print(self.content)
+        self.menu_options()
 
     def correct_input(self, string):
         user_input = input(string)
@@ -118,10 +120,25 @@ class MarkdownEditor:
         return user_input
 
     @staticmethod
-    def add_markup(content):
-        file = open("output.md", "w")
-        file.write(content)
-        file.close()
+    def input_number_rows(string):
+        user_input = input(string)
+        while user_input:
+            try:
+                user_input = int(user_input)
+            except ValueError:
+                print("Please input number")
+                user_input = input(string)
+                continue
+
+            else:
+                user_input = int(user_input)
+                if not 0 < user_input:
+                    print("Number rows cannot be negative")
+                    user_input = input(string)
+                    continue
+                break
+
+        return user_input
 
 
 mde = MarkdownEditor()
