d226f989624c0b97fde55e822a0400135e808eab
Yaroslav <y.b.grytsishyn@student.khai.edu>
2022-03-31 15:14:20 +0300

    currency_exchange {2-nd}

--- currency_exchange/currency_exchange.py
+++ currency_exchange/currency_exchange.py
@@ -1,4 +1,11 @@
+USD = 29.43
+EUR = 32.73
+UAH = 0.36
+
+print("Американский доллар - 1 mycoin =", USD, "USD\nЕвро - 1 mycoin =", EUR, "EUR\nУкраинская Гривна - 1 mycoin =", UAH, "UAH\n")
+
+
 user_input = float(input("Please, enter the number of mycoins you have: >"))
-exchange_input = float(input("Please, enter the exchange rate: >"))
-amount = float(user_input * exchange_input)
-print("The total amount of dollars:", amount)
+print("I will get", round(USD * user_input, 2), "USD from the sale of", user_input, "mycoins")
+print("I will get", round(EUR * user_input, 2), "EUR from the sale of", user_input, "mycoins")
+print("I will get", round(UAH * user_input, 2), "UAH from the sale of", user_input, "mycoins")
