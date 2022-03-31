ab5b219230e37269be5e0cfc1a30971a3320d641
Yaroslav <y.b.grytsishyn@student.khai.edu>
2022-03-31 14:27:38 +0300

    currency_exchange {1-st}

---
+++ currency_exchange/currency_exchange.py
@@ -0,0 +1,4 @@
+user_input = float(input("Please, enter the number of mycoins you have: >"))
+exchange_input = float(input("Please, enter the exchange rate: >"))
+amount = float(user_input * exchange_input)
+print("The total amount of dollars:", amount)
